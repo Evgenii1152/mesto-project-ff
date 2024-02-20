a1570173ad446f0bc05d304187f139bfb85c0e15
Evgenii Matveev <mathewsevgen@yandex.ru>
2024-02-20 14:26:55 +0300

    первое ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const content = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+initialCards.forEach(function (item) {
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
 
+  cardElement.querySelector(".card__image").src = item.link;
+  cardElement.querySelector(".card__title").textContent = item.name;
+  cardElement.querySelector(".card__image").alt = item.name;
+
+  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
+  cardDeleteButton.addEventListener("click", deleteCard);
+
+  content.append(cardElement);
+});
 // @todo: Функция удаления карточки
+function deleteCard() {
+  const deletListItem = event.target.closest(".card");
+  deletListItem.remove();
+}
 
 // @todo: Вывести карточки на страницу

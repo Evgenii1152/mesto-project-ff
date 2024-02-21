13770082df89112cf3e716f54919e24c34459ac1
Evgenii Matveev <mathewsevgen@yandex.ru>
2024-02-21 17:04:27 +0300

    первые исправления

--- scripts/index.js
+++ scripts/index.js
@@ -5,20 +5,22 @@ const cardTemplate = document.querySelector("#card-template").content;
 const content = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
-initialCards.forEach(function (item) {
+function createCard(link, name) {
   const cardElement = cardTemplate
     .querySelector(".places__item")
     .cloneNode(true);
 
-  cardElement.querySelector(".card__image").src = item.link;
-  cardElement.querySelector(".card__title").textContent = item.name;
-  cardElement.querySelector(".card__image").alt = item.name;
+  const cardImage = cardElement.querySelector(".card__image");
+  cardImage.src = link;
+  cardImage.alt = name;
+  cardElement.querySelector(".card__title").textContent = name;
+  
 
   const cardDeleteButton = cardElement.querySelector(".card__delete-button");
   cardDeleteButton.addEventListener("click", deleteCard);
 
   content.append(cardElement);
-});
+}
 // @todo: Функция удаления карточки
 function deleteCard() {
   const deletListItem = event.target.closest(".card");
@@ -26,3 +28,6 @@ function deleteCard() {
 }
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (item) {
+  createCard(item.link, item.name);
+});
